f2d14a65bcce806bb38f940a782e0f508f0682bd
Will Weiss <will.weiss1230@gmail.com>
2020-07-03 12:59:19 -0400

    avatar loads without a hitch

--- src/clients/github.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-
-export async function getUserByUsername(username: string): Promise<any> {
-  const result = await axios(`https://api.github.com/users/${username}`)
-  console.log('result', result)
-  return result.data
-}
--- src/components/application-form/github-input.tsx
+++ src/components/application-form/github-input.tsx
@@ -1,37 +1,71 @@
 import React from 'react'
+import axios from 'axios'
 import { last } from 'lodash'
 import { Avatar } from '@material-ui/core'
-import { GitHub, Person, FilterFrames } from '@material-ui/icons'
+import { GitHub } from '@material-ui/icons'
 import TextFieldWithIcon from './text-field-with-icon'
-import { getUserByUsername } from '../../clients/github'
 import debounceDefer from '../../utils/debounceDefer'
 
 
 
+async function getUserByUsername(username: string): Promise<any> {
+  const result = await axios(`https://api.github.com/users/${username}`)
+  return result.data
+}
+
 const debouncedGetUserByUserName = debounceDefer(getUserByUsername, 500)
 
 const githubIcon = <GitHub style={{ width: 35, height: 35 }} />
+
+// Loads the avatar of a given username, if present. Until an avatar has loaded, display the default github icon.
+function GithubAvatar({ username }: { username: null | string }): JSX.Element {
+
+  const [avatar, setAvatar] = React.useState<null | React.ReactNode>(null)
+  const [avatarLoaded, setAvatarLoaded] = React.useState(false)
+
+  React.useEffect(() => {
+    if (username) {
+      setAvatar(<Avatar
+        style={{ width: 35, height: 35 }}
+        src={`https://github.com/${username}.png?size=71`}
+        imgProps={{
+          onLoad: () => setAvatarLoaded(true)
+        }}
+      />)
+    } else {
+      setAvatar(null)
+      setAvatarLoaded(false)
+    }
+  }, [username])
+
+  // The display: 'none' thing is to put the underlying <img> on the page, forcing it to load
+  return (
+    <>
+      {!avatarLoaded && avatar && <div style={{ display: 'none' }}>{avatar}</div>}
+      {(avatarLoaded && avatar) ? avatar : githubIcon}
+    </>
+  )
+}
 
 type GithubInputProps = {
   checking: boolean
   setChecking(checking: boolean): void
 }
 
-export default function GithubInput({ checking, setChecking }: GithubInputProps) {
+
+export default function GithubInput({ checking, setChecking }: GithubInputProps): JSX.Element {
 
   const [testingUsername, setTestingUsername] = React.useState<string>('')
   const [confirmedUsername, setConfirmedUsername] = React.useState<null | string>(null)
-  const [avatar, setAvatar] = React.useState<null | React.ReactNode>(null)
-  const [avatarLoaded, setAvatarLoaded] = React.useState(false)
+
   const [blur, setBlur] = React.useState(false)
   const [error, setError] = React.useState(false)
   const [helperText, setHelperText] = React.useState<null | string>(null)
 
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     const username = last(event.target.value.split('/'))!
-    setAvatar(null)
-    setAvatarLoaded(false)
     setTestingUsername(username)
+    setConfirmedUsername(null)
     setError(false)
     setHelperText(null)
     setChecking(true)
@@ -41,29 +75,21 @@ export default function GithubInput({ checking, setChecking }: GithubInputProps)
     debouncedGetUserByUserName(testingUsername)
       .then(user => {
         if (user && user.avatar_url) {
-          console.log('in here')
           setConfirmedUsername(testingUsername)
-          setAvatar(<Avatar
-            style={{ width: 35, height: 35 }}
-            src={`https://github.com/${testingUsername}.png?size=71&_=${new Date().getTime()}`}
-            imgProps={{
-              onLoad: () => {
-                console.log('YES')
-                setAvatarLoaded(true)
-              }
-            }}
-          />)
           setError(false)
           setHelperText(null)
           setChecking(false)
           return
         }
-        throw new Error(`User ${testingUsername} not found`)
+        throw new Error(`404: User ${testingUsername} not found`)
       })
       .catch(err => {
-        console.error(err)
-        setConfirmedUsername(null)
-        setChecking(false)
+        if (err.message.includes('404')) {
+          setConfirmedUsername(null)
+          setChecking(false)
+        } else {
+          throw err
+        }
       })
   }, [testingUsername])
 
@@ -78,25 +104,16 @@ export default function GithubInput({ checking, setChecking }: GithubInputProps)
   }, [blur, testingUsername, confirmedUsername, checking])
 
   return (
-    <>
-      {
-        avatar && !avatarLoaded && (
-          <div style={{ display: 'none' }}>
-            {avatar}
-          </div>
-        )
-      }
-      <TextFieldWithIcon
-        label="Github Username"
-        name="githubUsername"
-        variant="outlined"
-        onChange={onChange}
-        onFocus={() => setBlur(false)}
-        onBlur={() => setBlur(true)}
-        error={error}
-        helperText={helperText}
-        startIcon={(avatarLoaded && avatar) ? avatar : githubIcon}
-      />
-    </>
+    <TextFieldWithIcon
+      label="Github Username"
+      name="githubUsername"
+      variant="outlined"
+      onChange={onChange}
+      onFocus={() => setBlur(false)}
+      onBlur={() => setBlur(true)}
+      error={error}
+      helperText={helperText}
+      startIcon={<GithubAvatar username={confirmedUsername} />}
+    />
   )
 }
