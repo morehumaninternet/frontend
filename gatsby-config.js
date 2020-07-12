a998414ece22a5ceb4a76803eada6eeee2806334
Will Weiss <will.weiss1230@gmail.com>
2020-07-12 15:52:43 -0400

    meta edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `More Human Internet`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A collaborative community creating a public platform for users and maintainers of the web`,
+    author: `@morehumaninter1`,
   },
   plugins: [
     {
