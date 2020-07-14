9f21b11c49adcc75c079dc3df0ec6071d632b8dd
Will Weiss <will.weiss1230@gmail.com>
2020-07-14 14:11:13 -0400

    rm gatsby-image & gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,6 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
