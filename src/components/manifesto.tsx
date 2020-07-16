ec673768151172c968392dcd2be5dc17252438f2
Will Weiss <will.weiss1230@gmail.com>
2020-07-16 13:52:22 -0400

    favicon + copy tweaks

--- src/components/manifesto.tsx
+++ src/components/manifesto.tsx
@@ -5,16 +5,13 @@ import { Link } from 'gatsby'
 export default () => (
   <>
     <p>
-      The internet sucks. Links are broken, images don’t load, navigation is difficult, requests go slowly, and pages are inaccessible. But its problems go deeper than that. Large corporations have taken control of a once open system. While they have brought efficiency, the platforms they have built are for their benefit, not ours. Big Tech firms make money from ads by using crypto-surveillance coupled with neural network models built by machine learning and not human design. These systems pander to and amplify our basest impulses and fears to get us to click, contributing to a polarized political climate where even basic facts cannot be agreed upon. We have quite literally created a cyborg superorganism that no one knows how to turn off.
+      The internet has lost our trust. We’re frustrated with its many issues — links are broken, images don’t load, navigation is difficult, requests go slowly, pages are inaccessible — but its problems go deeper than that. Large corporations have taken control of a once open system. While they have brought efficiency, the platforms they have built are for their benefit, not ours. Online crypto-surveillance feeds petabytes of data into neural networks to serve personalized ads. These systems, dictated more by machine learning than human design, pander to and amplify our basest impulses and fears to get us to click, contributing to a polarized political climate where even basic facts cannot be agreed upon. We have quite literally created a cyborg superorganism that no one knows how to turn off.
     </p>
     <p>
-      We need a more human internet, where its users and maintainers work together to fix its issues. The people who maintain the web desperately want to build fulfilling websites for those who use them. What if there were a public platform and collaborative community allowing users and maintainers of the web to have constructive conversations to make it better? Would such an experiment in openness and civility have positive effects beyond those participating in it?
+      We need a more human internet, where its users and maintainers work together to fix its issues and rebuild trust. We are founding a More Human Internet, a nonprofit and free public platform, as an online space for this open and civil dialogue. Website maintainers will be able to triage issues reported by, solicit feedback from, announce planned improvements to, and have constructive conversations with their users. In turn, users will be able to report issues for, give feedback to, request improvements from, and have constructive conversations with maintainers of websites. In addition to improving the design, accessibility, and experience of the web, we believe that this exercise will .
     </p>
     <p>
-      We are founding a More Human Internet, a nonprofit and free public platform, to find out. Maintainers will be able to triage issues reported by, solicit feedback from, announce planned improvements to, and have constructive conversations with their users to improve the design, accessibility, and experience of their websites. In turn, users will be able to report issues for, give feedback to, request improvements from, and have constructive conversations with maintainers to create more fulfilling online spaces.
-    </p>
-    <p>
-      We are assembling an inclusive team of motivated and talented volunteers to build this open platform and collaborative community. If you value teamwork and believe in this mission, we would love to work with you.
+      We are assembling an inclusive team of motivated and talented volunteers to build this public platform and collaborative community. If you value teamwork and believe in this mission, we would love to work with you.
     </p>
     <p className="lets-create">
       <Link to="/apply" className="human-blue"><strong>Let’s create a More Human Internet.</strong></Link>
