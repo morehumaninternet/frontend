9ad34415180e4420fc599d99107faad3708f3bcb
Will Weiss <will.weiss1230@gmail.com>
2020-07-24 17:29:21 -0400

    issue body loads html, shows toolbar on selection

--- src/components/widget/editor.tsx
+++ src/components/widget/editor.tsx
@@ -6,10 +6,24 @@ type EditorProps = {
   setIssueTitle(issueTitle: string): void
 }
 
+const defaultValue = `
+<strong>Steps</strong>
+
+<ol>
+<li>Looking for services</li>
+<li>Something else</li>
+</ol>
+
+<strong>Observations</strong>
+<br>
+<br>
+<strong>Expectations</strong>
+`
+
 
 export default function Editor({ issueTitle, setIssueTitle }: EditorProps): JSX.Element {
   const issueTitleRef = React.useRef<HTMLDivElement>()
-  // const issueBodyRef = React.useRef<HTMLDivElement>()
+  const issueBodyRef = React.useRef<HTMLDivElement>()
 
   React.useEffect(() => {
     const editorElement = issueTitleRef.current!.querySelector('trix-editor') as any
@@ -22,7 +36,29 @@ export default function Editor({ issueTitle, setIssueTitle }: EditorProps): JSX.
 
       setIssueTitle(match[1])
     })
-  })
+  }, [])
+
+  React.useEffect(() => {
+    const editorElement = issueBodyRef.current!.querySelector('trix-editor') as any
+
+    editorElement.editor.loadHTML(defaultValue)
+
+    // editorElement.editor
+    editorElement.addEventListener('trix-change', (event: { target: { value: string } }) => {
+
+    })
+
+    editorElement.addEventListener('trix-selection-change', () => {
+      const [selectionStart, selectionEnd] = editorElement.editor.getSelectedRange()
+      const toolbarElement = issueBodyRef.current!.querySelector('trix-toolbar') as any
+
+      if (selectionStart === selectionEnd) {
+        toolbarElement.style.display = 'none'
+      } else {
+        toolbarElement.style.display = 'block'
+      }
+    })
+  }, [])
 
   return (
     <div className="more-human-internet-widget-editor">
@@ -31,11 +67,17 @@ export default function Editor({ issueTitle, setIssueTitle }: EditorProps): JSX.
         className="more-human-internet-widget-editor-issue-title-input"
         dangerouslySetInnerHTML={{ __html: `<trix-editor placeholder="What is your issue?"></trix-editor>` }}
       />
-      {/* <div
+      <div
         ref={issueBodyRef as any}
         className="more-human-internet-widget-editor-issue-body-input"
-        dangerouslySetInnerHTML={{ __html: `<trix-editor></trix-editor>` }}
-      /> */}
+        dangerouslySetInnerHTML={{
+          __html: `
+            <trix-editor>
+              ${defaultValue}
+            </trix-editor>
+          `
+        }}
+      />
     </div>
   )
 }
