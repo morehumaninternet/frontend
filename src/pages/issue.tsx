9dac05342069b4225badfbed6fcdb0fe4db2f39e
Will Weiss <will.weiss1230@gmail.com>
2020-07-24 11:20:18 -0400

    more work on issue

--- src/pages/issue.tsx
+++ src/pages/issue.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Hero from '../components/hero'
+import { Avatar } from '@material-ui/core'
 
+
+
+function IssueBreadcrumbs() {
+  return (
+    <div className="issue-breadcrumbs">
+      <img src="/goalco.ico" /> goalco.com / Issues / 40305
+    </div>
+  )
+}
 
 
 export default function IssuePage(props: any): JSX.Element {
+
   return (
-    <Layout logoFade={1}>
+    <Layout
+      logoFade={1}
+      headerLinks={
+        <Avatar
+          src="github.com/will-weiss.png"
+        />
+      }
+    >
       <SEO pageTitle="Issue" />
+      <Hero additionalClassNames="issue">
+        <IssueBreadcrumbs  />
+      </Hero>
     </Layout>
   )
 }
