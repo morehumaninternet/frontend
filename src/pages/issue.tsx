c599f9d1f176b5f7fb6af59eebdc5b95477c28bb
Will Weiss <will.weiss1230@gmail.com>
2020-07-24 12:47:10 -0400

    inspect query params in effect

--- src/pages/issue.tsx
+++ src/pages/issue.tsx
@@ -17,27 +17,32 @@ function IssueBreadcrumbs({ site, issueId }: { site: string, issueId: number }) 
 
 type IssueState =
   | { loading: true }
-  | { loading: false, title: string }
+  | { loading: false, issue: { id: number, site: string, title: string } }
 
 
 export default function IssuePage(props: any): JSX.Element {
 
   const [issueState, setIssueState] = React.useState<IssueState>({ loading: true })
 
-  const params = new URLSearchParams(props.location.search)
-
-  const site = params.get('site')
-
-  if (!site) throw new Error('site required in query params')
-  const issueId = parseInt(params.get('id')!)
-
-  if (!issueId) throw new Error('issueId integer required in query params')
-
   React.useEffect(() => {
+    console.log('in here', props)
+    const params = new URLSearchParams(props.location.search)
+
+    const site = params.get('site')
+
+    if (!site) throw new Error('site required in query params')
+    const issueId = parseInt(params.get('id')!)
+
+    if (!issueId) throw new Error('issueId integer required in query params')
+
     mockApi.getIssueBySiteAndId(site, issueId).then(issue => {
       setIssueState({
         loading: false,
-        title: issue!.title
+        issue: {
+          id: issueId,
+          site: site,
+          title: issue!.title
+        }
       })
     })
   })
@@ -53,11 +58,13 @@ export default function IssuePage(props: any): JSX.Element {
     >
       <SEO pageTitle="Issue" />
       <Hero additionalClassNames="issue">
-        <IssueBreadcrumbs site={site} issueId={issueId} />
         {issueState.loading ? (
           <p>Loading...</p>
         ) : (
-          <h1>{issueState.title}</h1>
+          <>
+          <IssueBreadcrumbs site={issueState.issue.site} issueId={issueState.issue.id} />
+          <h1>{issueState.issue.title}</h1>
+          </>
         )}
       </Hero>
     </Layout>
