6630b7f5c6d1360358df7a67a02334602e90489c
Will Weiss <will.weiss1230@gmail.com>
2020-07-31 16:23:18 -0400

    add bit of second paragraph back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,6 +61,9 @@ export default class IndexPage extends React.Component<{ data: any }> {
             <p className="indent">
               The internet has lost our trust. We’re frustrated with its many issues — links are broken, images don’t load, navigation is difficult, requests go slowly, pages are inaccessible — but its problems go deeper than that. Large corporations have taken control of a once open system. While they have brought efficiency, the platforms they have built are for their benefit, not ours. Online crypto-surveillance feeds petabytes of data into neural networks to serve personalized ads. These systems, dictated more by machine learning than human design, pander to and amplify our basest impulses and fears to get us to click, contributing to a polarized political climate where even basic facts cannot be agreed upon. We have quite literally created a cyborg superorganism that no one knows how to turn off.
             </p>
+            <p className="indent">
+              We need a more human internet, where its users and maintainers work together to fix its issues and rebuild trust. We are founding a More Human Internet, a nonprofit and free public platform, as an online space for this open and civil dialogue.
+            </p>
             </div>
             <div className="manifesto-item manifesto-signature">
               <div className="signature-texts">
