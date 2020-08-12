5b4fd0fae67571362f3cf9c3cc3618c8f2daffb0
Will Weiss <will.weiss1230@gmail.com>
2020-08-12 13:47:14 -0400

    wip: trying gatsby-theme-i18n on the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,23 @@ import SEO from '../components/shared/seo'
 import DesignsShowcase from '../components/landing-page/designs-showcase'
 import ApplicationForm from '../components/landing-page/application-form'
 import setLogoFade from '../utils/setLogoFade'
-import { injectIntl, Link, FormattedMessage } from "gatsby-plugin-react-intl"
+import { LocalizedLink, LocalesList } from "gatsby-theme-i18n"
+import { useIntl } from "react-intl"
 
- class IndexPage extends React.Component<{ intl:any, data: any }> {
 
-  headerRef = React.createRef<HTMLDivElement>()
-  heroRef = React.createRef<HTMLDivElement>()  
-  
-  componentDidMount() {
-    const hero = this.heroRef.current!
+export default function IndexPage({ data }: { data: any }) {
+
+  const intl = useIntl()
+
+  const headerRef = React.useRef<HTMLDivElement>()
+  const heroRef = React.useRef<HTMLDivElement>()
+
+  React.useEffect(() => {
+    const hero = heroRef.current!
     let heroEndsAt = hero.offsetTop + hero.offsetHeight
 
     const onScroll = () => {
-      const headerIsFixed = window.getComputedStyle(this.headerRef.current!).position === 'fixed'
+      const headerIsFixed = window.getComputedStyle(headerRef.current!).position === 'fixed'
       if (!headerIsFixed) {
         return setLogoFade(0)
       }
@@ -41,61 +45,59 @@ import { injectIntl, Link, FormattedMessage } from "gatsby-plugin-react-intl"
     onResize()
     window.addEventListener('resize', onResize)
     window.addEventListener('scroll', onScroll)
-  }
+  }, [])
 
-  render(): JSX.Element {        
-    return (      
-      <Layout
-        mainClassName="index"
-        logoAgainstHero={true}
-        headerRef={this.headerRef}
-      >
-        <SEO />
-        <Hero additionalClassNames="index" heroRef={this.heroRef}>
-          <h1 className="white">{this.props.intl.formatMessage({ id: "Index-Title1" })}</h1>
-          <h2 className="white">{this.props.intl.formatMessage({ id: "Index-Title2" })}</h2>
-        </Hero>
-        <div className="posthero">
-          <DesignsShowcase />
-          <div className="manifesto">
-            <h1 className="manifesto-item">{this.props.intl.formatMessage({ id: "Index-Why1" })}</h1>
-            <div className="manifesto-item manifesto-contents">
-            <p className="indent">
-            {this.props.intl.formatMessage({ id: "Index-Why2" })}
-              
+  return (
+    <Layout
+      mainClassName="index"
+      logoAgainstHero={true}
+      headerRef={headerRef as any}
+    >
+      <SEO />
+      <Hero additionalClassNames="index" heroRef={heroRef as any}>
+        <h1 className="white">{intl.formatMessage({ id: "Index-Title1" })}</h1>
+        <h2 className="white">{intl.formatMessage({ id: "Index-Title2" })}</h2>
+      </Hero>
+      <div className="posthero">
+        <DesignsShowcase />
+        <div className="manifesto">
+          <h1 className="manifesto-item">{intl.formatMessage({ id: "Index-Why1" })}</h1>
+          <div className="manifesto-item manifesto-contents">
+          <p className="indent">
+            {intl.formatMessage({ id: "Index-Why2" })}
+          </p>
+          <p className="indent">
+            {intl.formatMessage({ id: "Index-Why3" })}
+          </p>
+          <p><strong>{intl.formatMessage({ id: "Index-Why4" })}</strong></p>
+          </div>
+          <div className="manifesto-item manifesto-signature">
+            <div className="signature-texts">
+              <img alt="Will Weiss signature" className="signature" src="signature.png"/>
+              <p>Will Weiss</p>
+            </div>
+            <div className="spacer"></div>
+            <a href="https://github.com/will-weiss">
+              <Img alt="Will Weiss portrait" className="avatar" fixed={data.willAvatar.childImageSharp.fixed} />
+            </a>
+          </div>
+          <h1 className="manifesto-item">{intl.formatMessage({ id: "Index-WeNeedYou1" })}</h1>
+          <div className="manifesto-item manifesto-contents">
+            <p>
+            {intl.formatMessage({ id: "Index-WeNeedYou2" })}
+
             </p>
-            <p className="indent">
-            {this.props.intl.formatMessage({ id: "Index-Why3" })}              
-            </p>
-            <p><strong>{this.props.intl.formatMessage({ id: "Index-Why4" })}</strong></p>
-            </div>
-            <div className="manifesto-item manifesto-signature">
-              <div className="signature-texts">
-                <img alt="Will Weiss signature" className="signature" src="signature.png"/>
-                <p>Will Weiss</p>
-              </div>
-              <div className="spacer"></div>
-              <a href="https://github.com/will-weiss">
-                <Img alt="Will Weiss portrait" className="avatar" fixed={this.props.data.willAvatar.childImageSharp.fixed} />
-              </a>
-            </div>
-            <h1 className="manifesto-item">{this.props.intl.formatMessage({ id: "Index-WeNeedYou1" })}</h1>
-            <div className="manifesto-item manifesto-contents">
-              <p>
-              {this.props.intl.formatMessage({ id: "Index-WeNeedYou2" })}
-                
-              </p>
-            </div>
-            <div className="manifesto-item form">
-              <ApplicationForm />
-            </div>
+          </div>
+          <div className="manifesto-item form">
+            <ApplicationForm />
           </div>
         </div>
-      </Layout>
-    )
-  }
+      </div>
+    </Layout>
+  )
 }
-export default injectIntl(IndexPage)
+
+
 export const query = graphql`
   query {
     willAvatar: file(relativePath: { eq: "will-weiss.jpg" }) {
