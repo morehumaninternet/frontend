b1d9e63d510fbb8980479962b5b07c298e695baf
Will Weiss <will.weiss1230@gmail.com>
2020-09-10 09:11:51 -0400

    first step of tour renders

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { ShepherdTour, ShepherdTourContext } from 'react-shepherd'
 import Widget from '../components/widget'
 import SEO from '../components/shared/seo'
 
@@ -7,24 +8,76 @@ import CartButton from '../components/demo-page/cart-button'
 import AddToCart from '../components/demo-page/add-to-cart'
 import Checkout from '../components/demo-page/checkout'
 
-export default function DemoPage(props: any): JSX.Element {
+const tourSteps = [
+  {
+    id: 'intro',
+    attachTo: '.more-human-internet-widget-boundary',
+    beforeShowPromise: function () {
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          window.scrollTo(0, 0)
+          resolve()
+        }, 500)
+      })
+    },
+    buttons: [
+      {
+        classes: 'shepherd-button-secondary',
+        text: 'Exit',
+        type: 'cancel',
+      },
+      {
+        classes: 'shepherd-button-primary',
+        text: 'Back',
+        type: 'back',
+      },
+      {
+        classes: 'shepherd-button-primary',
+        text: 'Next',
+        type: 'next',
+      },
+    ],
+    classes: 'custom-class-name-1 custom-class-name-2',
+    highlightClass: 'highlight',
+    scrollTo: false,
+    cancelIcon: {
+      enabled: true,
+    },
+    title: 'Welcome to React-Shepherd!',
+    text: [
+      'React-Shepherd is a JavaScript library for guiding users through your React app.',
+    ],
+    when: {
+      show: () => {
+        console.log('show step')
+      },
+      hide: () => {
+        console.log('hide step')
+      },
+    },
+  },
+]
+
+const tourOptions = {
+  defaultStepOptions: {
+    cancelIcon: {
+      enabled: true,
+    },
+  },
+  useModalOverlay: true,
+}
+
+function DemoPageContents(props: any): JSX.Element {
   const [checkout, setCheckout] = useState(false)
+  const tour = useContext(ShepherdTourContext)
+  console.log('tour', tour)
+
+  useEffect(() => {
+    tour?.start()
+  }, [])
 
   return (
-    <div className="demo-page">
-      <SEO
-        pageTitle="Demo"
-        links={[
-          {
-            rel: 'shortcut icon',
-            type: 'image/png',
-            sizes: '32x32',
-            href: '/goalco.ico',
-          },
-          { rel: 'stylesheet', type: 'text/css', href: '/trix.css' },
-        ]}
-        scripts={[{ type: 'text/javascript', src: '/trix.js' }]}
-      />
+    <>
       <header>
         <GoalCoLogo />
         <div className="links">
@@ -42,6 +95,29 @@ export default function DemoPage(props: any): JSX.Element {
         )}
       </div>
       <Widget navigate={props.navigate} />
+    </>
+  )
+}
+
+export default function DemoPage(props: any): JSX.Element {
+  return (
+    <div className="demo-page">
+      <SEO
+        pageTitle="Demo"
+        links={[
+          {
+            rel: 'shortcut icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/goalco.ico',
+          },
+          { rel: 'stylesheet', type: 'text/css', href: '/trix.css' },
+        ]}
+        scripts={[{ type: 'text/javascript', src: '/trix.js' }]}
+      />
+      <ShepherdTour steps={tourSteps as any} tourOptions={tourOptions}>
+        <DemoPageContents props={props} />
+      </ShepherdTour>
     </div>
   )
 }
