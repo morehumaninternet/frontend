02bd3f7db47749ecd073b259b2bea4329c9955d4
Will Weiss <will.weiss1230@gmail.com>
2020-09-15 17:11:26 -0400

    add parallax astronaut

--- src/components/new-landing-page/astronaut-star-group.tsx
+++ src/components/new-landing-page/astronaut-star-group.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function AstronautStarGroup(props: any): JSX.Element {
+export default function AstronautStarGroup(): JSX.Element {
   return (
-    <svg width={529} height={546} viewBox="0 0 529 546" fill="none" className="astronaut-star-group" {...props}>
+    <svg viewBox="0 0 529 546" fill="none" className="astronaut-star-group">
       <path d="M217.5 523.686c-4 2.4-4 4.5-4.5 6-.5-1-1-3.1-5-5.5 3-3 3.5-4 4.5-6 .5 1 1 3.1 5 5.5z" fill="#8BA0BB" fillOpacity={0.26} />
       <path d="M190.5 205.5c-4 2.4-4 4.5-4.5 6-.5-1-1-3.1-5-5.5 3-3 3.5-4 4.5-6 .5 1 1 3.1 5 5.5z" fill="#8BA0BB" fillOpacity={0.36} />
       <path d="M516.5 471.5c-4 2.4-4 4.5-4.5 6-.5-1-1-3.1-5-5.5 3-3 3.5-4 4.5-6 .5 1 1 3.1 5 5.5z" fill="#DFDFDF" fillOpacity={0.5} />
--- src/pages/new-landing-page.tsx
+++ src/pages/new-landing-page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { useIntl } from 'gatsby-plugin-intl'
 import { Button } from '@material-ui/core'
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
 import { LayoutWithNewHeader } from '../components/shared/layout'
@@ -7,7 +8,8 @@ import Stars from '../components/new-landing-page/stars'
 import { MountainBackground1, MountainBackground2 } from '../components/new-landing-page/mountain-background'
 import MountainMidground from '../components/new-landing-page/mountain-midground'
 import MountainForeground from '../components/new-landing-page/mountain-foreground'
-import { useIntl } from 'gatsby-plugin-intl'
+import Astronaut from '../components/new-landing-page/astronaut'
+import AstronautStarGroup from '../components/new-landing-page/astronaut-star-group'
 
 export default function NewLandingPage(): JSX.Element {
   return (
@@ -47,7 +49,12 @@ export default function NewLandingPage(): JSX.Element {
             </div>
           </div>
         </div>
-        <div className="post-sky1"></div>
+        <div className="post-sky1">
+          <AstronautStarGroup />
+          <Parallax styleOuter={{ position: 'absolute', width: '100%', top: '-5%', left: '-12%' }} y={['40%', '-40%']}>
+            <Astronaut />
+          </Parallax>
+        </div>
       </LayoutWithNewHeader>
     </ParallaxProvider>
   )
