3bc5b9af9fe1c4d1680c6b172e1eed47dff7166c
barbara prusiewicz <barbara.prusiewicz@gmail.com>
2020-09-16 18:22:53 -1000

    Sync devdiva acknowledgement with tour step

--- src/components/issue-page/index.tsx
+++ src/components/issue-page/index.tsx
@@ -2,8 +2,7 @@
 import React from 'react'
 import IssueMetadata from './overview'
 import IssueTimelineView, { IssueTimelineViewProps } from './timeline'
-import * as tourArgs from './tour'
-import { useTour } from '../../effects/useTour'
+import { useTour, withNextButton } from '../../effects/useTour'
 
 export type LoadedIssueContentProps = {
   avatarUrl?: string
@@ -13,27 +12,41 @@ export type LoadedIssueContentProps = {
 }
 
 function useDemo(issue: Issue, postComment: LoadedIssueContentProps['postComment'], changeStatus: LoadedIssueContentProps['changeStatus']): void {
-  if (issue.status === 'opened') {
-    setTimeout(() => {
-      changeStatus({ username: 'devdiva', avatarUrl: '/devdiva.png' }, 'acknowledged', {
-        html: `<div id="diva-acknowledged">I am able to reproduce this on our end, sorry about that! We'll get working on a fix right away</div>`,
-      })
-    }, 3000)
-  }
-
   if (issue.status === 'acknowledged') {
     setTimeout(() => {
       changeStatus({ username: 'devdiva', avatarUrl: '/devdiva.png' }, 'closed', {
         html: `<div>Deployed a fix that seems to be fixing the issue. Definitely leave a comment and reopen this issue if you're still being affected!</div>`,
       })
-    }, 6000)
+    }, 3000)
   }
 }
 
 export default function LoadedIssueContent({ avatarUrl, issue, postComment, changeStatus }: LoadedIssueContentProps): JSX.Element {
   useDemo(issue, postComment, changeStatus)
-
-  const tour = useTour(tourArgs)
+  
+  const steps = Object.freeze([
+    {
+      text: ['The issue is now posted to the more human internet platform where the site’s maintainer can see and address it.'],
+      attachTo: {
+        element: '.issue-timeline',
+        on: 'top',
+      },
+      ...withNextButton,
+    },
+    {
+      beforeShowPromise(): Promise<any> {
+        return changeStatus({ username: 'devdiva', avatarUrl: '/devdiva.png' }, 'acknowledged', {
+          html: `<div id="diva-acknowledged">I am able to reproduce this on our end, sorry about that! We'll get working on a fix right away</div>`,
+        })
+      },
+      text: ['Looks like Devdiva22 was online and quickly addressed the issue in near real-time.'],
+      attachTo: {
+        element: '#diva-acknowledged',
+        on: 'top',
+      },
+    },
+  ])
+  const tour = useTour({ steps: steps })
 
   return (
     <div className="issue-body">
