e8952b13840558bdbbc523823876ed9c409f5209
Will Weiss <will.weiss1230@gmail.com>
2020-09-20 10:41:17 -0400

    use circle to collapse star

--- src/components/new-landing-page/svgs/stars.tsx
+++ src/components/new-landing-page/svgs/stars.tsx
@@ -1,86 +1,42 @@
 import React from 'react'
 
+function Star({ cx, cy, r, durationSeconds }: { cx: number; cy: number; r: number; durationSeconds: number }): JSX.Element {
+  return (
+    <circle cx={cx} cy={cy} r={r} fill="#fff">
+      <animate attributeName="opacity" values="0.25;0.75;0.25" dur={`${durationSeconds}s`} repeatCount="indefinite" />
+    </circle>
+  )
+}
+
 export default function Stars(props: any): JSX.Element {
   return (
     <svg viewBox="0 0 1358 566" fill="none" className="stars" {...props}>
-      <ellipse cx={737.215} cy={442.206} rx={3.62631} ry={3.62757} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.1s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={404.77} cy={176.5} rx={3.49879} ry={3.5} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.2s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={156.925} cy={350.5} rx={2.49913} ry={2.5} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.3s" repeatCount="indefinite" />
-      </ellipse>
-      <circle cx={1252.41} cy={40.3603} r={1.36034} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.4s" repeatCount="indefinite" />
-      </circle>
-      <circle cx={43.8252} cy={1.36034} r={1.36034} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.5s" repeatCount="indefinite" />
-      </circle>
-      <circle cx={26.831} cy={370.36} r={1.36034} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.6s" repeatCount="indefinite" />
-      </circle>
-      <ellipse cx={1221.008} cy={271.814} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.7s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={249.207} cy={4.81379} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.8s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={347.174} cy={538.814} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.7s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={381.161} cy={133.814} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.6s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={731.04} cy={25.814} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.5s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={392.158} cy={563.814} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.4s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={599.272} cy={393} rx={1.99931} ry={2} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.3s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={661.877} cy={558.628} rx={3.62631} ry={3.62757} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.2s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={4.10581} cy={527.628} rx={3.62631} ry={3.62757} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.1s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={47.7303} cy={241.267} rx={2.26645} ry={2.26723} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={394.074} cy={301.183} rx={3.17303} ry={3.17413} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.1s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={1015.3} cy={46.1741} rx={3.17303} ry={3.17413} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.2s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={1247.55} cy={491.5} rx={1.49948} ry={1.5} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.3s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={1354.73} cy={236.721} rx={2.71974} ry={2.72068} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.4s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={278.65} cy={473.267} rx={2.26645} ry={2.26723} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.5s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={135.247} cy={137.489} rx={1.81316} ry={1.81379} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.6s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={1241.55} cy={306.5} rx={2.49913} ry={2.5} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.7s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={1083.1} cy={523} rx={1.99931} ry={2} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.8s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={1022.21} cy={272.081} rx={4.0796} ry={4.08102} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.7s" repeatCount="indefinite" />
-      </ellipse>
-      <ellipse cx={951.149} cy={554} rx={2.99896} ry={3} fill="#fff">
-        <animate attributeName="opacity" values="0.25;0.75;0.25" dur="3.6s" repeatCount="indefinite" />
-      </ellipse>
+      <Star cx={737.215} cy={442.206} r={3.62757} durationSeconds={3.1} />
+      <Star cx={404.77} cy={176.5} r={3.5} durationSeconds={3.2} />
+      <Star cx={156.925} cy={350.5} r={2.5} durationSeconds={3.3} />
+      <Star cx={1252.41} cy={40.3603} r={1.36034} durationSeconds={3.4} />
+      <Star cx={43.8252} cy={1.36034} r={1.36034} durationSeconds={3.5} />
+      <Star cx={26.831} cy={370.36} r={1.36034} durationSeconds={3.6} />
+      <Star cx={1221.008} cy={271.814} r={1.81379} durationSeconds={3.7} />
+      <Star cx={249.207} cy={4.81379} r={1.81379} durationSeconds={3.8} />
+      <Star cx={347.174} cy={538.814} r={1.81379} durationSeconds={3.7} />
+      <Star cx={381.161} cy={133.814} r={1.81379} durationSeconds={3.6} />
+      <Star cx={731.04} cy={25.814} r={1.81379} durationSeconds={3.5} />
+      <Star cx={392.158} cy={563.814} r={1.81379} durationSeconds={3.4} />
+      <Star cx={599.272} cy={393} r={2} durationSeconds={3.3} />
+      <Star cx={661.877} cy={558.628} r={3.62757} durationSeconds={3.2} />
+      <Star cx={4.10581} cy={527.628} r={3.62757} durationSeconds={3.1} />
+      <Star cx={47.7303} cy={241.267} r={2.26723} durationSeconds={3} />
+      <Star cx={394.074} cy={301.183} r={3.17413} durationSeconds={3.1} />
+      <Star cx={1015.3} cy={46.1741} r={3.17413} durationSeconds={3.2} />
+      <Star cx={1247.55} cy={491.5} r={1.5} durationSeconds={3.3} />
+      <Star cx={1354.73} cy={236.721} r={2.72068} durationSeconds={3.4} />
+      <Star cx={278.65} cy={473.267} r={2.26723} durationSeconds={3.5} />
+      <Star cx={135.247} cy={137.489} r={1.81379} durationSeconds={3.6} />
+      <Star cx={1241.55} cy={306.5} r={2.5} durationSeconds={3.7} />
+      <Star cx={1083.1} cy={523} r={2} durationSeconds={3.8} />
+      <Star cx={1022.21} cy={272.081} r={4.08102} durationSeconds={3.7} />
+      <Star cx={951.149} cy={554} r={3} durationSeconds={3.6} />
     </svg>
   )
 }
