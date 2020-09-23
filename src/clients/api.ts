12014377b6198a6acecc1531d42cd5005824ef59
Shachar Langer <shachar.langer@gmail.com>
2020-09-23 19:40:55 +0300

    Initial Algolia integration with the widget's search

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -1,10 +1,30 @@
+import algoliasearch from 'algoliasearch'
+import { createIssue } from './mockApi' // TODO - move this function to somewhere outside mockApi
+
 import { issueFromJson } from './util'
 
+// TODO - do something if they are not defined
+const client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_SEARCH_ONLY_API_KEY)
+const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME)
+
 export async function searchIssues({ site, title }: { site: string; title?: string }): Promise<ReadonlyArray<Issue>> {
-  // const issue = issueFromJson(localStorage.getItem(key)!)
-  throw new Error('Implement!')
+  const result = await index.search(title, { filters: `site:"${site}"` })
+  const issues = result.hits.map(hit => {
+    const { objectID, ...rest } = hit
+    return issueFromJson(JSON.stringify({ id: objectID, ...rest })) // TODO - remove access attributes from rest
+  })
+
+  return issues
 }
 
 export async function postIssue(issuePostBody: IssuePostBody): Promise<Issue> {
-  throw new Error('Implement!')
+  const issue = createIssue(issuePostBody)
+  const issueStr = JSON.stringify(issue)
+
+  await fetch('/api/postIssue', {
+    method: 'POST',
+    body: issueStr,
+  }) // TODO - do we want to handle any errors here?
+
+  return issue
 }
