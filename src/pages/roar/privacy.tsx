82ce6ede40fd5c93c0cff289253e6eeab494b694
Will Weiss <will.weiss1230@gmail.com>
2021-01-12 09:41:18 -0500

    actual privacy policy content

--- src/pages/roar/privacy.tsx
+++ src/pages/roar/privacy.tsx
@@ -4,27 +4,26 @@ import PrivacyTosPage from '../../components/roar/privacy-tos-page'
 export default function PrivacyPage(): JSX.Element {
   return (
     <PrivacyTosPage>
-      <h1>Terms & Conditions</h1>
-      <h3>1. License Grant</h3>
-      <p>We grant you a worldwide, non-exclusive and non-transferable right to use our services.</p>
+      <h1>Privacy Policy</h1>
+      <h3>1. Information Collection & Usage</h3>
+      <p>We collect your twitter username, profile image, and email address by receiving this information when you authenticate with Twitter.
+      We track websites you visit within the extension so that we can find the appropriate Twitter handle. Whenever you click the extension, a secure HTTPS request is made to our server. No personally identifying information is sent in that request that could tie back to you and no record is made of your having visited the site on our server.
+      When you post feedback, we use the permission you have previously granted to post a tweet using your account. The public tweet contents are exactly as they appeared to you in the extension. We save a record of the exact URLs you visited when taking each screenshot, but do not post those publicly.</p>
 
-      <h3>2. Support</h3>
-      <p>If you are in need of support, contact us at <a target="_blank" href="mailto:support@morehumaninternet.org">support@morehumaninternet.org</a>.</p>
+      <h3>2. Third Party Relationships</h3>
+      <p>This extension posts to Twitter using your account. By using More Human Internet’s services, you agree not to violate any of the policies set out in Twitter’s <a target="_blank" href="https://twitter.com/en/privacy">privacy policy</a> or <a target="_blank" href="https://twitter.com/en/tos">terms of service</a>.</p>
 
-      <h3>3. Copyright Infringement</h3>
-      <p>If you believe that our Services have been used in a way that constitutes copyright infringement, you should follow the process outlined here: <a target="_blank" href="https://www.dmca.com/faq/Report-copyright-infringement">https://www.dmca.com/faq/Report-copyright-infringement</a>.</p>
+      <h3>3. Cookies Policy</h3>
+      <p>We use cookies to keep you authorized on Twitter.</p>
 
-      <h3>4. Disclaimer of Warranties</h3>
-      <p>Our Service is provided “as is.” We hereby disclaim all warranties of any kind, express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose and non-infringement. We make no warranty that our Services will be error-free or that access thereto will be continuous or uninterrupted. You understand that you download from, or otherwise obtain content or services through, our Services at your own discretion and risk.</p>
+      <h3>4. Transfer of Business</h3>
+      <p>If ownership of all or substantially all of our business changes, or we undertake a corporate reorganization (including merger or consolidation) or any other action or transfer between More Human Internet entities, you expressly consent to More Human Internet transferring your information to the new owner or successor entity so that we can continue providing our services. If required, More Human Internet will notify the applicable data protection agency in each jurisdiction of such a transfer in accordance with the notification procedures under applicable data protection laws.</p>
 
-      <h3>5. Limitation of Liability</h3>
-      <p>In no event will we be liable with respect to any subject matter of this Agreement under any contract, negligence, strict liability or other legal or equitable theory for: (i) any special, incidental or consequential damages; (ii) the cost of procurement for substitute products or services; (iii) for interruption of use or loss or corruption of data; (iv) for any amounts that exceed the fees paid by you to us under this agreement during the twelve (12) month period prior to the cause of action. We shall have no liability for any failure or delay due to matters beyond their reasonable control. The foregoing shall not apply to the extent prohibited by applicable law.</p>
+      <h3>5. Updates to Privacy Policy</h3>
+      <p>We may update this Privacy Policy to reflect changes in our information practices. If we make any material changes, we will provide notice on this website, and we may notify you by email (sent to the email address specified in your account), prior to the change becoming effective. We encourage you to periodically review this page for the latest information on our privacy practices. If you continue to use the Services after those changes are in effect, you agree to the revised policy.</p>
 
-      <h3>6. Termination</h3>
-      <p>We may terminate your access to all or any part of our Services at any time, with or without cause, with or without notice, effective immediately. If you wish to terminate this Agreement, you may simply discontinue using our services. All provisions of this Agreement which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
-
-      <h3>7. Governing Law</h3>
-      <p>The Agreements (and any non-contractual disputes / claims arising out of or in connection with them) are subject to the laws of the state of Delaware, United States of America, without regard to choice or conflicts of law principles.</p>
+      <h3>6. Dispute Resolution</h3>
+      <p>If you have a complaint about More Human Internet’s privacy policies, please let us know at <a target="_blank" href="mailto:privacy@morehumaninternet.org">privacy@morehumaninternet.org</a>.</p>
     </PrivacyTosPage>
   )
 }
