1074be6ea3bf2eb16d766893afda218962b979f3
Will Weiss <will.weiss1230@gmail.com>
2021-01-15 01:06:36 -0500

    byebye Rickroll

--- src/components/roar/landing-page/how-it-wroks/video.tsx
+++ src/components/roar/landing-page/how-it-wroks/video.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = ({ videoSrcURL, videoTitle }: { videoSrcURL: string; videoTitle: string }) => (
   <div className="video">
     <iframe
-      src={`${videoSrcURL}?autoplay=1&mute=1`}
+      src={videoSrcURL}
       title={videoTitle}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       frameBorder="0"
