2efb5e9bd7408f72c76e50531d3cceb942c20ba3
Shachar Langer <shachar.langer@gmail.com>
2021-01-28 22:32:53 +0200

    Add ROAR_SERVER env var

--- src/components/spy/index.tsx
+++ src/components/spy/index.tsx
@@ -2,59 +2,59 @@
 import React, { useState } from 'react'
 
 const SpyPage = (): JSX.Element => {
-  const [url, setUrl] = useState<string>('')
-  const [spyList, setSpyList] = useState<ReadonlyArray<string>>([])
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<string>('')
+    const [url, setUrl] = useState<string>('')
+    const [spyList, setSpyList] = useState<ReadonlyArray<string>>([])
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string>('')
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault()
-    setError('')
-    setSpyList([])
-    setLoading(true)
-    let response, data
-    try {
-      const urlWithProtocol = url.startsWith('http') ? url : `http://${url}`
-      response = await fetch(`https://localhost:5004/v1/spy/?url=${urlWithProtocol}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      data = await response.json()
-    } catch (error) {
-      console.error(error)
-      setLoading(false)
-      setError('Error while scanning for spies')
-      return
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault()
+        setError('')
+        setSpyList([])
+        setLoading(true)
+        let response, data
+        try {
+            const urlWithProtocol = url.startsWith('http') ? url : `http://${url}`
+            response = await fetch(`${process.env.ROAR_SERVER}/v1/spy/?url=${urlWithProtocol}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            data = await response.json()
+        } catch (error) {
+            console.error(error)
+            setLoading(false)
+            setError('Error while scanning for spies')
+            return
+        }
+        setLoading(false)
+        if (!response || !response.ok) {
+            setError('Error while scanning for spies')
+            return
+        } else {
+            if (data.spies) {
+                setSpyList(data.spies.join(', '))
+            } else {
+                setError('Could not find any spies')
+            }
+        }
     }
-    setLoading(false)
-    if (!response || !response.ok) {
-      setError('Error while scanning for spies')
-      return
-    } else {
-      if (data.spies) {
-        setSpyList(data.spies.join(', '))
-      } else {
-        setError('Could not find any spies')
-      }
-    }
-  }
 
-  return (
-    <div className="spy-container">
-      <div className="wrapper">
-        <h1>Who's been spying on me?</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="url" placeholder="https://..." value={url} onChange={e => setUrl(e.target.value)} />
-          <input className="mhi-button" type="submit" value="analyze" />
-        </form>
-        {loading && <p>Please wait...</p>}
-        {error && <p>{error}</p>}
-        {spyList && <p>{spyList}</p>}
-      </div>
-    </div>
-  )
+    return (
+        <div className="spy-container">
+            <div className="wrapper">
+                <h1>Who's been spying on me?</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="url" placeholder="https://..." value={url} onChange={e => setUrl(e.target.value)} />
+                    <input className="mhi-button" type="submit" value="analyze" />
+                </form>
+                {loading && <p>Please wait...</p>}
+                {error && <p>{error}</p>}
+                {spyList && <p>{spyList}</p>}
+            </div>
+        </div>
+    )
 }
 
 export default SpyPage
