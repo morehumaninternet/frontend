e71798870948a70be4620aae4a517b11c8d72eb7
hanliu12 <hanliu12@publicisgroupe.net>
2022-03-01 14:59:37 -0500

    correct missing whitespace error - wip

--- src/components/shared/mhi-header-logos.tsx
+++ src/components/shared/mhi-header-logos.tsx
@@ -12,19 +12,19 @@ const phrases: ReadonlyArray<string> = [
   'Live Your Values',
 ]
 
-const AnimateWordsArray = [{color:'', words:['']}, {color:'', words: ['']}, {color:'', words: ['']}]
+const AnimateWordsArray = [{color:'',words:['']},{color:'',words:['']},{color:'',words: ['']}]
 
 const createWordGroups = function(phrasesArray : ReadonlyArray<string>) {
   phrasesArray.forEach((phrase, index) => {
-    let wordsArray = phrase.split(' ') 
+    let wordsArray = phrase.split(' ')
     if (wordsArray.length !== 3) {
       throw new Error (`The ${index}th phrase (${phrase}) need to be exact 3 words`)
     }
     wordsArray.forEach((word, i) => {
         if(i === 0 || i === 2) {
-          AnimateWordsArray[i].color= 'human-blue' 
+          AnimateWordsArray[i].color= 'human-blue'
         }else if (i === 1) {
-          AnimateWordsArray[i].color= 'human-pink' 
+          AnimateWordsArray[i].color= 'human-pink'
         }
         AnimateWordsArray[i].words[0] ? AnimateWordsArray[i].words.push(word) : AnimateWordsArray[i].words[0] = word
     })
