ab5e809a8d0bd726858adfb1beaa5c250e59c9dc
Will Weiss <will.weiss1230@gmail.com>
2022-03-31 15:08:03 -0400

    mobile layout for causes

--- src/components/new-landing-page/causes/index.tsx
+++ src/components/new-landing-page/causes/index.tsx
@@ -3,85 +3,58 @@ import { BigCircle, SmallCircle } from '../../shared/circles'
 import Info from './info'
 
 
+/*
+  - Create a component for the circles
+  - Create a component for the text content
+  - Implement proper images & text content
+  - Use single column layout on mobile
+*/
+
+const CauseImage = ({ src }: { src: string }) => (
+  <div className="cause__image">
+    <img src={src} />
+  </div>
+)
+
+const CauseTextContent = ({ heading, description }: { heading: string, description: string }) => (
+  <div className="cause__text-content">
+    <div>
+      <h2>{heading}</h2>
+      <p>{description}</p>
+    </div>
+  </div>
+)
+
+const Cause = ({ imgSrc, heading, description }: { imgSrc: string, heading: string, description: string }) => (
+  <>
+    <CauseImage src={imgSrc} />
+    <CauseTextContent
+      heading={heading}
+      description={description}
+    />
+  </>
+)
 
 const Causes = forwardRef(
   (_, ref): JSX.Element => {
     return (
       <section className="causes" ref={ref as any}>
         <div className="causes__content">
-          <div
-            style={{
-              gridRow: '1/4',
-              gridColumn: 1,
-              display: 'grid',
-              placeItems: 'center'
-            }}
-          >
-            <img
-              src="/dsa_hero.png"
-              style={{
-                borderRadius: '50%',
-                border: '3px solid red'
-              }}
-            />
-          </div>
-
-          <div style={{ gridRow: '2', gridColumn: 3, display: 'grid', placeItems: 'center', padding: 10 }}>
-            <div>
-              <h2>Developing African Leaders Through Sports</h2>
-              <p>Developing African Leaders Through Sports</p>
-            </div>
-          </div>
-
-          <div
-            style={{
-              gridRow: '3/6',
-              gridColumn: 3,
-              display: 'grid',
-              placeItems: 'center'
-            }}
-          >
-            <img
-              src="/dsa_hero.png"
-              style={{
-                borderRadius: '50%',
-                border: '3px solid red'
-              }}
-            />
-          </div>
-
-          <div style={{ gridRow: '4', gridColumn: 1, display: 'grid', placeItems: 'center', padding: 10 }}>
-            <div>
-              <h2>Developing African Leaders Through Sports</h2>
-              <p>Developing African Leaders Through Sports</p>
-            </div>
-          </div>
-
-          <div
-            style={{
-              gridRow: '5/8',
-              gridColumn: 1,
-              display: 'grid',
-              placeItems: 'center'
-            }}
-          >
-            <img
-              src="/dsa_hero.png"
-              style={{
-                borderRadius: '50%',
-                border: '3px solid red'
-              }}
-            />
-          </div>
-
-          <div style={{ gridRow: '6', gridColumn: 3, display: 'grid', placeItems: 'center', padding: 10 }}>
-            <div>
-              <h2>Developing African Leaders Through Sports</h2>
-              <p>Developing African Leaders Through Sports</p>
-            </div>
-          </div>
-
-
+          <Cause
+            imgSrc="/dsa_hero.png"
+            heading="Developing African Leaders Through Sports"
+            description="Developing African Leaders Through Sports"
+          />
+          <Cause
+            imgSrc="/dsa_hero.png"
+            heading="Developing African Leaders Through Sports"
+            description="Developing African Leaders Through Sports"
+          />
+          <Cause
+            imgSrc="/dsa_hero.png"
+            heading="Developing African Leaders Through Sports"
+            description="Developing African Leaders Through Sports"
+          />
         </div>
       </section>
     )
