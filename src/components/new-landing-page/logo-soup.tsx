613079f3bcfe6c042b473936b77f6133cdb5b074
hankeliu2015 <hanke.liu@gmail.com>
2022-04-04 17:09:08 -0400

    change the logo-soup font size as h2, remove customized fonts size for desktop and phone

--- src/components/new-landing-page/logo-soup.tsx
+++ src/components/new-landing-page/logo-soup.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LogoSoup(): JSX.Element {
     return (
         <div className="logo-soup">
-            <h3 className="soup__header">Streamline your operations and reach more donors with the latest technology</h3>
+            <h2 className="soup__header">Streamline your operations and reach more donors with the latest technology</h2>
             <div className="soup__content">
                 <img src="/logo-soup-1.png" />
                 <img src="/logo-soup-gatsby.png" />
