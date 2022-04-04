6a209b4656f08df2d3e8babc42b0de13417abb2b
hankeliu2015 <hanke.liu@gmail.com>
2022-04-04 17:33:27 -0400

    change file name for donorbox png file. Use auto-fit and grid-gap for grid system

--- src/components/new-landing-page/logo-soup.tsx
+++ src/components/new-landing-page/logo-soup.tsx
@@ -5,7 +5,7 @@ export default function LogoSoup(): JSX.Element {
         <div className="logo-soup">
             <h2 className="soup__header">Streamline your operations and reach more donors with the latest technology</h2>
             <div className="soup__content">
-                <img src="/logo-soup-1.png" />
+                <img src="/logo-soup-donorbox.png" />
                 <img src="/logo-soup-gatsby.png" />
                 <img src="/logo-soup-webflow.png" />
                 <img src="/logo-soup-zoom.png" />
