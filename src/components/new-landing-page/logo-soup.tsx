d33183cd60c48415e662a647f31cc40f4cc4e8db
hankeliu2015 <hanke.liu@gmail.com>
2022-04-04 22:03:05 -0400

    replace previous donorbox logo and put all technologies logos under a folder

--- src/components/new-landing-page/logo-soup.tsx
+++ src/components/new-landing-page/logo-soup.tsx
@@ -5,14 +5,14 @@ export default function LogoSoup(): JSX.Element {
         <div className="logo-soup">
             <h2 className="soup__header">Streamline your operations and reach more donors with the latest technology</h2>
             <div className="soup__content">
-                <img src="/logo-soup-donorbox.png" />
-                <img src="/logo-soup-gatsby.png" />
-                <img src="/logo-soup-webflow.png" />
-                <img src="/logo-soup-zoom.png" />
-                <img src="/logo-soup-gdrive.png" />
-                <img src="/logo-soup-react.png" />
-                <img src="/logo-soup-notion.png" />
-                <img src="/logo-soup-slack.png" />
+                <img src="/technologies/donorbox.png" />
+                <img src="/technologies/gatsby.png" />
+                <img src="/technologies/webflow.png" />
+                <img src="/technologies/zoom.png" />
+                <img src="/technologies/gdrive.png" />
+                <img src="/technologies/react.png" />
+                <img src="/technologies/notion.png" />
+                <img src="/technologies/slack.png" />
             </div>
         </div>
     )
