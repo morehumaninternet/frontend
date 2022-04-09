5d19833554b4e15f0f577267f10d2d1e13db795c
hankeliu2015 <hanke.liu@gmail.com>
2022-04-09 16:30:54 -0400

    add basic styles file for join section

--- src/components/new-landing-page/join-section.tsx
+++ src/components/new-landing-page/join-section.tsx
@@ -4,8 +4,8 @@ import {ApplicationForm} from '../shared/application-form'
 export default React.forwardRef(
   (_, ref): JSX.Element => {
     return (
-      <div ref={ref as any}>
-        <h2>Join Our Community</h2>
+      <div className = "join-community" ref={ref as any}>
+        <h2 className="join-header" >Join Our Community</h2>
         <ApplicationForm />
       </div>
     )
