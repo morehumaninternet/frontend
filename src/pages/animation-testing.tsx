85dddebab1c1cc49546d2a8e40b4339b38b42951
hankeliu2015 <hanke.liu@gmail.com>
2022-04-09 19:09:01 -0400

    add animatin-testing page to testing roatting-logo component

---
+++ src/pages/animation-testing.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {MHIRotatingLogo} from '../components/new-landing-page/Rotating-Logo'
+export default () => {
+    return (
+        <div>
+            <h2>Animation Testing - Rotating Logos</h2>
+            <MHIRotatingLogo />
+        </div>
+    )
+}
