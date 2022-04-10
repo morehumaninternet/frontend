732d6cc43fd282de5a5ee89cc5efd2bd73405e2c
hankeliu2015 <hanke.liu@gmail.com>
2022-04-09 20:41:46 -0400

    wrap layout for animation page

--- src/pages/animation-testing.tsx
+++ src/pages/animation-testing.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import {MHIRotatingLogo} from '../components/new-landing-page/Rotating-Logo'
+import {MHIRotatingLogo} from '../components/new-landing-page/rotating-Logo'
+import Hero from '../components/new-landing-page/hero'
+import {Layout} from '../components/shared/layout'
 
 export default () => {
     return (
-        <div>
+        <Layout>
             <h2>Animation Testing - Rotating Logos</h2>
             <MHIRotatingLogo />
-        </div>
+        </Layout>
     )
 }
