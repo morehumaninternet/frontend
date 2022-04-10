23cdff222c7ee68a6676857fe1464d26ba48aa0a
hankeliu2015 <hanke.liu@gmail.com>
2022-04-09 21:40:37 -0400

    add inline styles for animation testing page

--- src/pages/animation-testing.tsx
+++ src/pages/animation-testing.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import {MHIRotatingLogo} from '../components/new-landing-page/rotating-Logo'
 import Hero from '../components/new-landing-page/hero'
 import {Layout} from '../components/shared/layout'
+import { blue } from '@material-ui/core/colors'
 
 export default () => {
     return (
         <Layout>
-            <h2>Animation Test - Rotating Logos</h2>
-            <div className="new-hero">
+            <h2 style={{color: '#164176', textAlign: 'center', padding: '2rem 0'}}>
+                Animation Testing Page - Rotating Logos
+            </h2>
+            <div className="new-hero" style={{padding: '4rem 0'}}>
                 <MHIRotatingLogo />
             </div>
         </Layout>
