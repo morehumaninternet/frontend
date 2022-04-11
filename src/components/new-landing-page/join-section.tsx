24adbedc6d661e197449dca8f64f47c30b21d3ff
hankeliu2015 <hanke.liu@gmail.com>
2022-04-11 10:16:01 -0400

    use existing css classes for join-section styles

--- src/components/new-landing-page/join-section.tsx
+++ src/components/new-landing-page/join-section.tsx
@@ -4,9 +4,11 @@ import {ApplicationForm} from '../shared/application-form'
 export default React.forwardRef(
   (_, ref): JSX.Element => {
     return (
-      <div className = "join-community" ref={ref as any}>
-        <h2 className="join-header" >Join Our Community</h2>
-        <ApplicationForm />
+      <div className="join-community">
+        <div className ="join" ref={ref as any}>
+          <h2 className="mhi-heading human-blue" >Join Our Community</h2>
+          <ApplicationForm />
+        </div>
       </div>
     )
   }
