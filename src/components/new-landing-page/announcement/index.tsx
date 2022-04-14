047e5300815614a69e1661fd1ad1d7b55d897969
Shachar Langer <shachar.langer@gmail.com>
2022-04-14 22:44:39 +0300

    Fix linting

--- src/components/new-landing-page/announcement/index.tsx
+++ src/components/new-landing-page/announcement/index.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Announcement = (): JSX.Element => {
   return (
     <div className="announcement-container">
-        <img src="/announcement.png" />
-        <div className="announcement__text">
-            <h2>Stewarding Hawaii’s cultural and natural resources for generations</h2>
-            <p>Livable Hawaii Kai Hui</p>
-            <a href="#">Learn more</a>
-        </div>
+      <img src="/announcement.png" />
+      <div className="announcement__text">
+        <h2>Stewarding Hawaii’s cultural and natural resources for generations</h2>
+        <p>Livable Hawaii Kai Hui</p>
+        <a href="#">Learn more</a>
+      </div>
     </div>
   )
 }
 
-export default Announcement 
+export default Announcement
