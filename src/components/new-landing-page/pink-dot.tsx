6d5a6b4925753966748a6d149f3f7fa5bc998a06
hankeliu2015 <hanke.liu@gmail.com>
2022-04-20 15:46:28 -0400

    add pink-dot component and dot-pink.png file for leaders section - wip

---
+++ src/components/new-landing-page/pink-dot.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PinkDot(): JSX.Element {
+    return(
+        <div>
+            <p>More Human Internet partners with international causes to deepen their impact and outreach</p>
+            <img src="/leaders/dot-pink.png"></img>
+        </div>
+    )
+}
