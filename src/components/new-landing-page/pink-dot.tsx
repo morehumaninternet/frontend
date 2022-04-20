a634ec53534890895b87e0aeae6c2816ed29e0cd
hankeliu2015 <hanke.liu@gmail.com>
2022-04-20 16:54:11 -0400

    postion pink-dot component - wip

--- src/components/new-landing-page/pink-dot.tsx
+++ src/components/new-landing-page/pink-dot.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function PinkDot(): JSX.Element {
     return(
-        <div>
-            <p>More Human Internet partners with international causes to deepen their impact and outreach</p>
-            <img src="/leaders/pink-dot.png"></img>
+        <div className="pink-dot-container">
+            <h2 className='pink-dot-text'>More Human Internet partners with international causes to deepen their impact and outreach</h2>
+            <img className="pink-dot" src="/leaders/pink-dot.png" />
         </div>
     )
 }
