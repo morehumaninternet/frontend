f04fc3ede91646295960d082944ba8b18ba029ab
hankeliu2015 <hanke.liu@gmail.com>
2022-04-20 17:02:17 -0400

    fix build error

--- src/components/new-landing-page/pink-dot.tsx
+++ src/components/new-landing-page/pink-dot.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function PinkDot(): JSX.Element {
     return(
-        <div className='pink-dot-container'>
-            <h2 className='pink-dot-text'>More Human Internet partners with international causes to deepen their impact and outreach</h2>
-            <img className='pink-dot' src="/leaders/pink-dot.png" />
+        <div className="pink-dot-container">
+            <h2 className="pink-dot-text">More Human Internet partners with international causes to deepen their impact and outreach</h2>
+            <img className="pink-dot" src="/leaders/pink-dot.png" />
         </div>
     )
 }
