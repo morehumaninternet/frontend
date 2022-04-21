ff958ba5d82621a491cb68daf1a9e81b00f4f7c7
hankeliu2015 <hanke.liu@gmail.com>
2022-04-21 00:46:28 -0400

    add styles for pink-dot section on leaders component

--- src/components/new-landing-page/pink-dot.tsx
+++ src/components/new-landing-page/pink-dot.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PinkDot(): JSX.Element {
     return(
         <div className="pink-dot-container">
-            <h2 className="pink-dot-text">More Human Internet partners with international causes to deepen their impact and outreach</h2>
+            <h3 className="pink-dot-text">More Human Internet partners with international causes to deepen their impact and outreach</h3>
             <img className="pink-dot" src="/leaders/pink-dot.png" />
         </div>
     )
