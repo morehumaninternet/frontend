a810292927dd1d5b7207564a93abd79c4d2014c0
hankeliu2015 <hanke.liu@gmail.com>
2022-04-27 00:15:45 -0400

    rename pink-dot component css classes

--- src/components/new-landing-page/pink-dot.tsx
+++ src/components/new-landing-page/pink-dot.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function PinkDot(): JSX.Element {
     return(
-        <div className="pink-dot-container">
-            <h3 className="pink-dot-text">More Human Internet partners with international causes to deepen their impact and outreach</h3>
+        <div className="pink_dot__wrapper">
+            <h3 className="pink_dot__text">More Human Internet partners with international causes to deepen their impact and outreach</h3>
         </div>
     )
 }
