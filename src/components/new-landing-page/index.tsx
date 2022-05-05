034b5cd3febbff4585a57adb06e559058b1fb5bc
Shachar Langer <shachar.langer@gmail.com>
2022-05-05 22:20:51 +0300

    Re-arrange meta tags

--- src/components/new-landing-page/index.tsx
+++ src/components/new-landing-page/index.tsx
@@ -22,12 +22,13 @@ const NewLandingPage = ({ location }: PageProps): JSX.Element => {
       header={<Header location={location} internalSectionRefs={internalSectionRefs} />}
     >
       <SEO
-        meta={[{ property: 'og:image', itemprop: 'image', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/mhi-og-image.png' },
-        { name: 'twitter:image', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/mhi-og-image.png' },
-        { property: 'og:image:type', content: 'image/png' },
-        { property: 'og:image:height', content: '613' },
-        { property: 'og:image:width', content: '1219' },
-        { property: 'og:url', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/' }
+        meta={[
+          { property: 'og:image', itemprop: 'image', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/mhi-og-image.png' },
+          { property: 'og:image:type', content: 'image/png' },
+          { property: 'og:image:height', content: '613' },
+          { property: 'og:image:width', content: '1219' },
+          { property: 'og:url', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/' },
+          { name: 'twitter:image', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/mhi-og-image.png' }
         ]}
       />
       <Hero />
