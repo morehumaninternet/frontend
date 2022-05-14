18294281d3d47e319f504765c783cbf087ae2a11
Hank Liu <hanke.liu@gmail.com>
2022-05-14 03:24:25 -0400

    adjust pink-dot position relative to blue-section - wip

--- src/components/shared/pink-dot.tsx
+++ src/components/shared/pink-dot.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function PinkDot(): JSX.Element {
     return(
         <div className="pink_dot__wrapper">
-            <h3 className="pink_dot__text">More Human Internet partners with international causes to deepen their impact and outreach</h3>
+            <div className="pink_dot">
+                <h3 className="pink_dot__text">More Human Internet partners with international causes to deepen their impact and outreach</h3>
+            </div>
         </div>
     )
 }
