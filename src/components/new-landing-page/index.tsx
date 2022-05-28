ca3abf43e56736bf796558e93744e646d6fec49e
Shachar Langer <shachar.langer@gmail.com>
2022-05-28 21:19:27 +0300

    Remove itemprop

--- src/components/new-landing-page/index.tsx
+++ src/components/new-landing-page/index.tsx
@@ -23,7 +23,7 @@ const NewLandingPage = ({ location }: PageProps): JSX.Element => {
     >
       <SEO
         meta={[
-          { property: 'og:image', itemprop: 'image', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/mhi-og-image.png' },
+          { property: 'og:image', content: 'https://deploy-preview-200--admiring-carson-42ff06.netlify.app/mhi-og-image.png' },
           // { property: 'og:image:type', content: 'image/png' },
           // { property: 'og:image:height', content: '613' },
           // { property: 'og:image:width', content: '1219' },
