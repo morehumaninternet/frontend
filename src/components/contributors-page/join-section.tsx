586f5d572750b252aa0d6c01a5e9b402d86d3ffe
Hank Liu <hanke.liu@gmail.com>
2022-05-31 23:22:58 -0400

    contributors application form testing text field

--- src/components/contributors-page/join-section.tsx
+++ src/components/contributors-page/join-section.tsx
@@ -25,6 +25,36 @@ export default React.forwardRef(
                 }}
               />
             </FormGroup>
+            <FormGroup>
+              <TextField
+                label="What skills would you hope to bring?"
+                name="skills"
+                variant="outlined"
+                required
+                multiline
+                rows={5}
+                InputProps={{
+                  inputProps: {
+                    'aria-label': 'What skills would you hope to bring?',
+                  },
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <TextField
+                label="What are you hoping to learn?"
+                name="learn"
+                variant="outlined"
+                required
+                multiline
+                rows={5}
+                InputProps={{
+                  inputProps: {
+                    'aria-label': 'What are you hoping to learn?',
+                  },
+                }}
+              />
+            </FormGroup>
           </ApplicationForm>
         </div>
       </section>
