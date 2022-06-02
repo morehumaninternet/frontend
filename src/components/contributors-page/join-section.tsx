0ed3268f5dc462c77f30ecca4f57d0b7e43b97c7
hankeliu2015 <hanke.liu@gmail.com>
2022-06-02 00:18:11 -0400

    rename contributor formName back to join-mhi

--- src/components/contributors-page/join-section.tsx
+++ src/components/contributors-page/join-section.tsx
@@ -9,7 +9,7 @@ export default React.forwardRef(
         <div>
           <h1 className="mhi-heading human-blue">Join our team</h1>
           <p><i>We'd love to work with you! We'll reach out to folks who apply to schedule a 30 minute interview to find a great cause you can support!</i></p>
-          <ApplicationForm formName="join-mhi-contributors">
+          <ApplicationForm formName="join-mhi">
             <FormGroup>
               <TextField
                 label="What causes motivate you?"
